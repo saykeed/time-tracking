85142d8e79f6fe37c9cbfa18e150b18b73fe2a11
Abubakri Sherif <75755419+saykeed@users.noreply.github.com>
2021-12-10 22:49:04 +0100

    Finished the design for mobile screen

--- js/track.js
+++ js/track.js
@@ -1,10 +1,34 @@
 
 let databox = document.querySelector("#data");
-
+let dailybtn = document.querySelector("#daily");
+let weeklybtn = document.querySelector("#weekly");
+let monthlybtn = document.querySelector("#monthly");
+let allBtn = document.querySelectorAll("#button p");
 
 let showData = function (x) {
+
+    for (let i = 0; i < allBtn.length; i++) {
+        allBtn[i].style.color = "white";
+     }
+
     let template = "";
     let timeframe = "";
+    let currentHours = "";
+    let prevHours = "";
+    
+    
+
+    if (x === "daily") {
+        dailybtn.style.color = "red";
+        timeframe += 'Yesterday';
+    } else if (x === "weekly") {
+        weeklybtn.style.color = "red";
+        timeframe += 'Last Week';  
+    } else if (x === "monthly") {
+        monthlybtn.style.color = "red";
+        timeframe += 'Last Month';  
+    }
+
     let req = new XMLHttpRequest();
     req.open("GET", "json/data.json");
     
@@ -12,22 +36,28 @@ let showData = function (x) {
         
         let ourData = JSON.parse(req.responseText);
         
+        
+
         ourData.forEach( (data)  => {
-            if (x === "daily") {
-                timeframe += 'Yesterday';
-            } else if (x === "weekly") {
-                timeframe += 'Last Week';  
-            } else if (x === "monthly") {
-                timeframe += 'Last Month';  
+            if (data.timeframes[x].current >= 2) {
+                currentHours = "hrs";
+            } else{
+                currentHours = "hr"
             }
-            
+
+            if (data.timeframes[x].previous >= 2) {
+                prevHours = "hrs";
+            } else {
+                prevHours = "hr"
+            }
+
              template += `
                 <div id="backdiv">
                     <div id="timedata">
                         <p id="firstp">${data.title} <span><img src="images/icon-ellipsis.svg"></span></p>
                         <div id="difference">
-                            <h1>${data.timeframes[x].current}hrs</h1>
-                            <p>${timeframe} - ${data.timeframes[x].previous}hrs</p>
+                            <h1>${data.timeframes[x].current}${currentHours}</h1>
+                            <p>${timeframe} - ${data.timeframes[x].previous}${prevHours}</p>
                         </div>
                     </div>
                 </div>
@@ -40,7 +70,7 @@ let showData = function (x) {
     req.send();
 }
 
-window.onload = showData("monthly");
+window.onload = showData("weekly");
 
 /*
 
